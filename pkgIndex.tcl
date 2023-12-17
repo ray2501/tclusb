5fcfa4f3ac31de4c3791513b586414595dac7ce1
Danilo Chang <ray2501@gmail.com>
2023-12-17 10:46:05 +0800

    Update

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded tclusb 0.3 \
-    [list load [file join $dir libtclusb0.3.so] tclusb]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded tclusb 0.3 \
+	    [list load [file join $dir libtcl9tclusb0.3.so] [string totitle tclusb]]
+} else {
+    package ifneeded tclusb 0.3 \
+	    [list load [file join $dir libtclusb0.3.so] [string totitle tclusb]]
+}
